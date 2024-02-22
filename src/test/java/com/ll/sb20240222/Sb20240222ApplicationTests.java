62812f0e5efec8066feb21f81d8c97542518031e
jhs512 <jangka512@gmail.com>
2024-02-22 13:07:28 +0900

    work

--- src/test/java/com/ll/sb20240222/Sb20240222ApplicationTests.java
+++ src/test/java/com/ll/sb20240222/Sb20240222ApplicationTests.java
@@ -2,8 +2,10 @@ package com.ll.sb20240222;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class Sb20240222ApplicationTests {
 
     @Test
